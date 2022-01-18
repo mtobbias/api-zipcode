a0d97aacb5d0be52bfea6aa4baa74660b87ac45b
Marcelo Tobias <marcelo.tobbias@gmail.com>
2022-01-18 18:56:37 -0300

    initial commit

---
+++ src/main/java/br/dev/mtobias/api/zipcode/apizipcode/ApiZipcodeApplication.java
@@ -0,0 +1,13 @@
+package br.dev.mtobias.api.zipcode.apizipcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiZipcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiZipcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/dev/mtobias/api/zipcode/apizipcode/ApiZipcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.mtobias.api.zipcode.apizipcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiZipcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
