038b06ae9212168e21ccd391f9e6afdb5e0393c2
Marcelo Tobias <marcelo.tobbias@gmail.com>
2022-01-30 20:11:33 -0300

    commit #4

--- src/main/java/br/dev/mtobias/api/zipcode/apizipcode/ApiZipcodeApplication.java
+++ src/main/java/br/dev/mtobias/api/zipcode/apizipcode/ApiZipcodeApplication.java
@@ -3,6 +3,7 @@ package br.dev.mtobias.api.zipcode.apizipcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Marcelo Tobias <marcelo.tobbias@gmail.com>
@@ -10,7 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableFeignClients()
-//@EnableSwagger2
+@EnableSwagger2
 public class ApiZipcodeApplication {
 
     public static void main(String[] args) {
