a556ef1b430ff379ccbeaddb5ab88c2584531da4
Marcelo Tobias <marcelo.tobbias@gmail.com>
2022-01-30 20:26:08 -0300

    commit #7

--- src/main/java/br/dev/mtobias/api/zipcode/apizipcode/ApiZipcodeApplication.java
+++ src/main/java/br/dev/mtobias/api/zipcode/apizipcode/ApiZipcodeApplication.java
@@ -1,20 +1,34 @@
 package br.dev.mtobias.api.zipcode.apizipcode;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author Marcelo Tobias <marcelo.tobbias@gmail.com>
  *
  */
+@RequestMapping("/")
 @SpringBootApplication
 @EnableFeignClients()
-//@EnableSwagger2
-public class ApiZipcodeApplication {
+public class ApiZipcodeApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiZipcodeApplication.class, args);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/index.html");
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<String> index(){
+        return  ResponseEntity.ok("api-codezip");
     }
 
 }
