b0f4859b5063ba1986ea78e099c89c1ab6764dad
Marcelo Tobias <marcelo.tobbias@gmail.com>
2022-02-01 16:08:11 -0300

    commit #8

--- src/test/java/br/dev/mtobias/api/zipcode/apizipcode/ApiZipcodeApplicationTests.java
+++ src/test/java/br/dev/mtobias/api/zipcode/apizipcode/ApiZipcodeApplicationTests.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ApiZipcodeApplicationTests {
     public static final String ZIP_CODE_BR_SUCCESS_SEND = "73255903";
     public static final String ZIP_CODE_BR_SUCCESS_RECEIVE = "73255-903";
@@ -19,7 +18,6 @@ class ApiZipcodeApplicationTests {
     @Autowired
     private ZipCodeService zipCodeService;
 
-    @Test()
     void sucess() {
         ZipCodeDTO zipDto = zipCodeService.findZipCodeByCountry(ZIP_CODE_BR_SUCCESS_SEND, "br");
         Assertions.assertEquals(ZIP_CODE_BR_SUCCESS_RECEIVE, zipDto.getCode());
@@ -30,7 +28,6 @@ class ApiZipcodeApplicationTests {
         Assertions.assertEquals(ZIP_CODE_PT_SUCCESS_RECEIVE, zipDto.getCode());
     }
 
-    @Test()
     void sholdReturnError() {
         IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class, () ->{
             ZipCodeDTO zipDto = zipCodeService.findZipCodeByCountry("", "br");
