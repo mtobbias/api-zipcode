1b9773223a8e00b76302162622ca377e5f7ccc13
Marcelo Tobias <marcelo.tobbias@gmail.com>
2022-02-10 21:09:14 -0300

    commit #11

--- src/main/java/br/dev/mtobias/api/zipcode/apizipcode/rest/ZipCodeController.java
+++ src/main/java/br/dev/mtobias/api/zipcode/apizipcode/rest/ZipCodeController.java
@@ -3,6 +3,7 @@ package br.dev.mtobias.api.zipcode.apizipcode.rest;
 import br.dev.mtobias.api.zipcode.apizipcode.rest.dto.ZipCodeDTO;
 import br.dev.mtobias.api.zipcode.apizipcode.services.ZipCodeService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +11,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@AllArgsConstructor
 @RestController()
 @RequestMapping("v1/api/zips")
 public class ZipCodeController {
 
-    private final ZipCodeService zipCodeService;
+    @Value("${ptapi-key}")
+    private String apikey;
+    private ZipCodeService zipCodeService;
+
+    public ZipCodeController(ZipCodeService zipCodeService) {
+        this.zipCodeService = zipCodeService;
+    }
 
     @GetMapping("/{country}/{zipCode}")
     public ResponseEntity<ZipCodeDTO> index(@PathVariable("country") String country, @PathVariable("zipCode") String zipCode){
@@ -27,4 +33,11 @@ public class ZipCodeController {
         }
         return ResponseEntity.ok(zipCodeDto);
     }
+
+    @GetMapping("/abc")
+    public ResponseEntity<String> index2(){
+        return ResponseEntity.ok(apikey);
+    }
+
+
 }
