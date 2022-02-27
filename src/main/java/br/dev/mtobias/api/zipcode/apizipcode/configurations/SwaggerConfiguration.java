29e0b3061a23a8f070e657199f92b1d7ea2dca89
MARCELO TOBIAS LEITE DA SILVA <marcelo.silva@ilia.digital>
2022-02-27 16:18:24 -0300

    refactor

--- src/main/java/br/dev/mtobias/api/zipcode/apizipcode/configurations/SwaggerConfiguration.java
+++ src/main/java/br/dev/mtobias/api/zipcode/apizipcode/configurations/SwaggerConfiguration.java
@@ -2,13 +2,20 @@ package br.dev.mtobias.api.zipcode.apizipcode.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class SwaggerConfiguration {
+public class SwaggerConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/","/swagger-ui");
+    }
 
     @Bean
     public Docket fromApi(){
